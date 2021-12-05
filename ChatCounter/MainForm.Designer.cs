0219f69e63eeea5e339eb5c6e856bd1bb5a22960
ashlord666 <ashlord666@gmail.com>
2021-12-06 03:02:54 +0800

    Add project files.

---
+++ ChatCounter/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ChatCounter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusMsg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ChooseFileButton = new System.Windows.Forms.Button();
+            this.whatsAppChatFilename = new System.Windows.Forms.TextBox();
+            this.GenerateCSVButton = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusMsg});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 279);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(685, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusMsg
+            // 
+            this.statusMsg.Name = "statusMsg";
+            this.statusMsg.Size = new System.Drawing.Size(0, 17);
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Location = new System.Drawing.Point(77, 9);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDateTimePicker.TabIndex = 1;
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Location = new System.Drawing.Point(77, 45);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDateTimePicker.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "End Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File:";
+            // 
+            // ChooseFileButton
+            // 
+            this.ChooseFileButton.Location = new System.Drawing.Point(591, 79);
+            this.ChooseFileButton.Name = "ChooseFileButton";
+            this.ChooseFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ChooseFileButton.TabIndex = 6;
+            this.ChooseFileButton.Text = "Choose File";
+            this.ChooseFileButton.UseVisualStyleBackColor = true;
+            this.ChooseFileButton.Click += new System.EventHandler(this.ChooseFileButton_Click);
+            // 
+            // whatsAppChatFilename
+            // 
+            this.whatsAppChatFilename.Location = new System.Drawing.Point(77, 81);
+            this.whatsAppChatFilename.Name = "whatsAppChatFilename";
+            this.whatsAppChatFilename.ReadOnly = true;
+            this.whatsAppChatFilename.Size = new System.Drawing.Size(508, 20);
+            this.whatsAppChatFilename.TabIndex = 7;
+            // 
+            // GenerateCSVButton
+            // 
+            this.GenerateCSVButton.Location = new System.Drawing.Point(16, 118);
+            this.GenerateCSVButton.Name = "GenerateCSVButton";
+            this.GenerateCSVButton.Size = new System.Drawing.Size(650, 158);
+            this.GenerateCSVButton.TabIndex = 8;
+            this.GenerateCSVButton.Text = "Generate CSV";
+            this.GenerateCSVButton.UseVisualStyleBackColor = true;
+            this.GenerateCSVButton.Click += new System.EventHandler(this.GenerateCSVButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 301);
+            this.Controls.Add(this.GenerateCSVButton);
+            this.Controls.Add(this.whatsAppChatFilename);
+            this.Controls.Add(this.ChooseFileButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChatCounter";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusMsg;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ChooseFileButton;
+        private System.Windows.Forms.TextBox whatsAppChatFilename;
+        private System.Windows.Forms.Button GenerateCSVButton;
+    }
+}
+
---
+++ ChatCounter/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ChatCounter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        MainForm(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog chooseWhatsAppChatDialog = new OpenFileDialog();
+            chooseWhatsAppChatDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            chooseWhatsAppChatDialog.Multiselect = false;
+            chooseWhatsAppChatDialog.ShowDialog();
+            whatsAppChatFilename.Text = chooseWhatsAppChatDialog.FileName;
+            statusMsg.Text = "";
+        }
+
+        private void GenerateCSVButton_Click(object sender, EventArgs e)
+        {
+            ChooseFileButton.Enabled = false;
+            //statusMsg.Text = $"startDate: {startDateTimePicker.Value.ToShortDateString()}, endDate: {endDateTimePicker.Value.Date.ToShortDateString()}";
+
+            Regex rgx = new Regex(@"^(\d{2}/\d{2}/\d{4}),\s+\d{2}:\d{2}\s+-\s+([^:]+):.*$");
+            int totalcount = 0;
+            int matchedcount = 0;
+            int datematchedcount = 0;
+            var cultureInfo = new CultureInfo("en-US");
+            IDictionary<string, int> talkative = new Dictionary<string, int>();
+
+            try
+            {
+                if (endDateTimePicker.Value.Date < startDateTimePicker.Value.Date)
+                {
+                    MessageBox.Show("Don't make me insult your intelligence but your start and end dates don't make sense", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    statusMsg.Text = "";
+                }
+                else
+                {
+                    if (whatsAppChatFilename.Text.Trim().Length > 0)
+                    {
+                        if (File.Exists(whatsAppChatFilename.Text))
+                        {
+                            foreach (string line in File.ReadLines(whatsAppChatFilename.Text))
+                            {
+                                totalcount++;
+
+                                if (rgx.IsMatch(line))
+                                {
+                                    matchedcount++;
+                                    Match match = rgx.Match(line);
+
+                                    DateTime dateValue;
+                                    // Check date
+                                    // Parse date
+                                    if (DateTime.TryParseExact(match.Groups[1].Value, "dd/MM/yyyy", cultureInfo, DateTimeStyles.None, out dateValue))
+                                    {
+                                        if ((dateValue.Date > endDateTimePicker.Value.AddDays(1).Date) || (dateValue.Date < startDateTimePicker.Value.Date))
+                                        {
+                                            continue;
+                                        }
+                                    }
+
+                                    // Increment count
+                                    talkative.TryGetValue(match.Groups[2].Value, out var currentCount);
+                                    talkative[match.Groups[2].Value] = currentCount + 1;
+                                    datematchedcount++;
+                                }
+
+                            }
+
+                            var ordered = talkative.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+                            var outputfile = Path.Combine(Path.GetDirectoryName(whatsAppChatFilename.Text), $"{Path.GetFileNameWithoutExtension(whatsAppChatFilename.Text)}.csv");
+
+                            using (TextWriter tw = new StreamWriter(outputfile, append: false))
+                            {
+                                ordered.ToList().ForEach(x => tw.WriteLine($"{x.Key},{x.Value}"));
+                            }
+
+                            statusMsg.Text = $"{datematchedcount} lines filtered from {matchedcount} message. Processed {totalcount} lines. {ordered.ToList().Count} chatters.";
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("File doesn't exist...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            statusMsg.Text = "";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            ChooseFileButton.Enabled = true;
+        }
+    }
+}
